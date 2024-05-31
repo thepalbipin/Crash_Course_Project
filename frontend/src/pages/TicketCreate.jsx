5a30730f1dacffdea3b4b6763ce19d0cc2c0d56e
Bipin <bipinpal737@gmail.com>
2024-05-31 14:30:49 +0530

    delete function added

--- frontend/src/pages/TicketCreate.jsx
+++ frontend/src/pages/TicketCreate.jsx
@@ -1,5 +1,83 @@
+import { Container, Input, Textarea, VStack, Select, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export default function TicketCreate(){
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [assignee, setAssignee] = useState("");
+  const [status, setStatus] = useState("");
+  const [priority, setPriority] = useState("");
+  const navigate = useNavigate();
+
+  async function handleCreate(){
+    try {
+      const newTicket = {
+        title: title,
+        description: description,
+        assignee: assignee,
+        status: status,
+        priority: priority
+      };
+
+      let response = await axios({
+        method: "post",
+        url: `http://localhost:3000/tickets`,
+        data: newTicket,
+      });
+
+      console.log(response);
+
+      if(response.status === 201) {
+        navigate("/tickets");
+      }      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>TicketCreate</div>
+    <Container>
+      <VStack spacing={6} my={4}>
+        <Input 
+          placeholder='Enter Title'
+          size='lg'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          />
+        <Textarea 
+            placeholder='Enter Description'
+            size='lg'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            />
+        <Select placeholder='Assignee' size='lg' value={assignee} onChange={(e) => setAssignee(e.target.value)} >
+          <option value='malar'>Malar</option>
+          <option value='devi'>Devi</option>
+          <option value='sakshi'>Sakshi</option>
+        </Select>
+        <Select placeholder='Status' size='lg' value={status} onChange={(e) => setStatus(e.target.value)} >
+          <option value='completed'>Completed</option>
+          <option value='progress'>Progress</option>
+          <option value='pending'>Pending</option>
+        </Select>
+        <Select placeholder='Priority' size='lg' value={priority} onChange={(e) => setPriority(Number(e.target.value))} >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9</option>
+        </Select>
+        <Button
+          colorScheme='red' 
+          variant='solid'
+          onClick={handleCreate}>Create Ticket</Button>
+      </VStack>
+    </Container>
   )
 }
--- frontend/src/pages/TicketView.jsx
+++ frontend/src/pages/TicketView.jsx
@@ -1,5 +1,143 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Card, 
+  CardHeader, 
+  CardBody, 
+  Heading, 
+  Stack,
+  StackDivider, 
+  Text, 
+  CardFooter,
+  Button,
+  Box,
+  HStack
+} from "@chakra-ui/react";
+
+
+import LoadingIndicator from '../components/LoadingIndicator';
+import ErrorIndicator from '../components/ErrorIndicator';
+
+
+
 export default function TicketView(){
+  const {id} = useParams();
+
+  const navigate = useNavigate();
+
+  const [loading, setLoading] = useState(false);
+  const [ticket, setTicket] = useState({});
+  const [error, setError] = useState(false);
+
+  async function fetchAndUpdateData(id){
+    setLoading(true);
+    try {
+      let response = await axios({
+        method: "get",
+        url: `http://localhost:3000/tickets/${id}`,
+      });
+
+      let data = response?.data;
+      setLoading(false)
+      setTicket(data);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+      console.log(error);
+    }
+  }
+
+    useEffect(()=>{
+      fetchAndUpdateData(id);
+    }, [id]);
+
+    async function deleteTicket(){
+      try {
+        let response = await axios({
+          method: "delete",
+          url: `http://localhost:3000/tickets/${id}`,
+        });
+
+        if(response.status === 200){
+          navigate('/tickets');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if(loading){
+      return <LoadingIndicator />
+    }
+
+    if(error){
+      return <ErrorIndicator />
+    }
+
+    const {title, status, description, assignee, priority} = ticket;
   return (
-    <div>TicketView</div>
+    <>
+      <Card>
+        <CardHeader>
+          <Heading size='md'>{title}</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <Stack divider={<StackDivider />} spacing='4'>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Status
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {status}
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Priority
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {priority}
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Description
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {description}
+              </Text>
+            </Box>
+            <Box>
+              <Heading size='xs' textTransform='uppercase'>
+                Assignee
+              </Heading>
+              <Text pt='2' fontSize='sm'>
+                {assignee}
+              </Text>
+            </Box>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <HStack spacing={4}>
+            <Button 
+            variant='solid' 
+            colorScheme='red'
+            onClick={()=>{navigate(`/ticket/edit/${id}`)}}
+            >
+              Edit Ticket
+            </Button>
+            <Button 
+            variant='solid' 
+            colorScheme='red'
+            onClick={deleteTicket}
+            >
+              Delete Ticket
+            </Button>
+          </HStack>
+        </CardFooter>
+      </Card>
+    </>
   )
 }
